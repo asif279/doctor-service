e6dde590b8377f192be7645c5099e60c11990604
asif279 <asifofficial8130@gmail.com>
2023-03-27 17:36:18 +0600

    a

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -25,8 +25,7 @@ const SignUp = () => {
         }
         updateUser(userInfo)
         .then(() => {
-          navigate('/');
-          // ...
+        saveUser(data.name,data.email);
         }).catch((error) => {
           // An error occurred
           // ...
@@ -53,6 +52,25 @@ const handleGoogle =()=>{
     console.log(errorMessage);
     
   });
+}
+
+const saveUser=(name,email)=>{
+  const user={name,email};
+  fetch('http://localhost:5000/users',{
+    method:'POST',
+    headers:{
+      'content-type':'application/json'
+    },
+    body:JSON.stringify(user)
+  })
+  .then(res=>res.json())
+  .then(data=>{
+    console.log(data);
+    navigate('/');
+  
+  })
+
+
 }
 
 
